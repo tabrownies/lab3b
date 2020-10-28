b5590a5eb076681724fbb5326fffecb274aecd77
Timothy Brown <tabrownies@gmail.com>
2020-10-27 18:14:21 -0600

    Tutorials are working

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Browse from '../views/Browse.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/browse',
+    name: 'Browse',
+    component: Browse  
+  },
 ]
 
 const router = new VueRouter({
